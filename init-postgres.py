4a74ab9277650e3f0f7e1db0ad3458bca1ffd442
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-04-05 19:58:55 +0200

    Update path to files

--- init-postgres.py
+++ init-postgres.py
@@ -2,23 +2,23 @@ from collections import _OrderedDictItemsView
 import pandas as pd
 from sqlalchemy import create_engine
 
-customers = pd.read_csv("olist_customers_dataset.csv")
+customers = pd.read_csv("datasets/olist_customers_dataset.csv")
 
-geolocation = pd.read_csv("olist_geolocation_dataset.csv")
+geolocation = pd.read_csv("datasets/olist_geolocation_dataset.csv")
 
-order_items = pd.read_csv("olist_order_items_dataset.csv")
+order_items = pd.read_csv("datasets/olist_order_items_dataset.csv")
 
-order_payments = pd.read_csv("olist_order_payments_dataset.csv")
+order_payments = pd.read_csv("datasets/olist_order_payments_dataset.csv")
 
-order_reviews = pd.read_csv("olist_order_reviews_dataset.csv")
+order_reviews = pd.read_csv("datasets/olist_order_reviews_dataset.csv")
 
-orders = pd.read_csv("olist_orders_dataset.csv")
+orders = pd.read_csv("datasets/olist_orders_dataset.csv")
 
-products = pd.read_csv("olist_products_dataset.csv")
+products = pd.read_csv("datasets/olist_products_dataset.csv")
 
-sellers = pd.read_csv("olist_sellers_dataset.csv")
+sellers = pd.read_csv("datasets/olist_sellers_dataset.csv")
 
-product_category_names = pd.read_csv("product_category_name_translation.csv")
+product_category_names = pd.read_csv("datasets/product_category_name_translation.csv")
 
 
 engine = create_engine('postgresql://postgres:postgres@localhost:5051/postgres')
