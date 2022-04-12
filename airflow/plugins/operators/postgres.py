72860ab215763e8306d883c53313cfa1b4774d9f
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-04-12 14:31:37 +0200

    Add DataFrametoPostgresOverrideOperator

---
+++ airflow/plugins/operators/postgres.py
@@ -0,0 +1,24 @@
+from airflow.models.baseoperator import BaseOperator
+
+class DataFrametoPostgresOverrideOperator(BaseOperator):
+    template_fields = ('data',)
+
+    def __init__(
+        self,
+        table_name,
+        data,
+        connection_uri: str = "postgresql://postgres:postgres@warehouse-postgres:5432/postgres",
+        *args,
+        **kwargs
+    ):
+        super(DataFrametoPostgresOverrideOperator, self).__init__(*args, **kwargs)
+        self.connection_uri = connection_uri
+        self.table_name = table_name
+        self.data = data
+
+    def execute(self, context):
+        import pandas as pd
+        from sqlalchemy import create_engine
+
+        con = create_engine(self.connection_uri)
+        self.data.to_sql(name=self.table_name, con=con, if_exists="replace")
