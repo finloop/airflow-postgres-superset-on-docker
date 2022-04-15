72ebc9fe197dcd2990ba505849b4815eaf3acc16
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-04-15 16:28:27 +0200

    Add PostgresToDataFrameOperator

--- airflow/plugins/operators/postgres.py
+++ airflow/plugins/operators/postgres.py
@@ -1,6 +1,6 @@
 from airflow.models.baseoperator import BaseOperator
 
-class DataFrametoPostgresOverrideOperator(BaseOperator):
+class DataFrameToPostgresOverrideOperator(BaseOperator):
     template_fields = ('data',)
 
     def __init__(
@@ -11,7 +11,7 @@ class DataFrametoPostgresOverrideOperator(BaseOperator):
         *args,
         **kwargs
     ):
-        super(DataFrametoPostgresOverrideOperator, self).__init__(*args, **kwargs)
+        super(DataFrameToPostgresOverrideOperator, self).__init__(*args, **kwargs)
         self.connection_uri = connection_uri
         self.table_name = table_name
         self.data = data
@@ -22,3 +22,28 @@ class DataFrametoPostgresOverrideOperator(BaseOperator):
 
         con = create_engine(self.connection_uri)
         self.data.to_sql(name=self.table_name, con=con, if_exists="replace")
+
+
+class PostgresToDataFrameOperator(BaseOperator):
+
+    def __init__(
+        self,
+        table_name,
+        data,
+        connection_uri: str = "postgresql://postgres:postgres@warehouse-postgres:5432/postgres",
+        *args,
+        **kwargs
+    ):
+        super(PostgresToDataFrameOperator, self).__init__(*args, **kwargs)
+        self.connection_uri = connection_uri
+        self.table_name = table_name
+
+    def execute(self, context):
+        import pandas as pd
+        from sqlalchemy import create_engine
+
+        con = create_engine(self.connection_uri)
+        # WARNING. Never use this in production, it can be easly exploited with
+        # SQL injection.
+        df = pd.read_sql_query(f"select * from {self.table_name}",con=con)
+        return df
