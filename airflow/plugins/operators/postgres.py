a897bf0aa71a7d63bb3e39f7fcf0702fe48d595b
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-04-17 16:48:01 +0200

    Add CheckIfTableExistsOperator

--- airflow/plugins/operators/postgres.py
+++ airflow/plugins/operators/postgres.py
@@ -1,3 +1,4 @@
+from airflow.exceptions import AirflowFailException
 from airflow.models.baseoperator import BaseOperator
 
 class DataFrameToPostgresOverrideOperator(BaseOperator):
@@ -52,3 +53,28 @@ class PostgresToDataFrameOperator(BaseOperator):
         # SQL injection.
         df = pd.read_sql_query(f"select * from {self.table_name}",con=con)
         return df
+
+class CheckIfTableExistsOperator(BaseOperator):
+
+    def __init__(
+        self,
+        table_name,
+        connection_uri: str = "postgresql://postgres:postgres@warehouse-postgres:5432/postgres",
+        *args,
+        **kwargs
+    ):
+        super(CheckIfTableExistsOperator, self).__init__(*args, **kwargs)
+        self.connection_uri = connection_uri
+        self.table_name = table_name
+
+    def execute(self, context):
+        import pandas as pd
+        import numpy as np
+        from sqlalchemy import create_engine
+
+        con = create_engine(self.connection_uri)
+        # WARNING. Never use this in production, it can be easly exploited with
+        # SQL injection.
+        df = pd.read_sql_query(f"select * from pg_catalog.pg_tables;", con=con)
+        if np.sum(df["tablename"] == self.table_name) == 0:
+            raise AirflowFailException(f"Table {self.table_name} does not exist.")
