738a50a61861f2a606bc2d85fab3c38d9b89d56c
Piotr Krawiec <piotr.krawiec23@gmail.com>
2022-04-17 16:02:57 +0200

    Fix ML DAG

--- airflow/dags/dag_ml.py
+++ airflow/dags/dag_ml.py
@@ -1,32 +1,24 @@
 
-from asyncio import tasks
 from operators.postgres import DataFrameToPostgresOverrideOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.decorators import dag, task
 
 import pendulum
 
-# The DAG object; we'll need this to instantiate a DAG
-from airflow import DAG
-
-# Operators; we need this to operate! 
-from airflow.operators.python import PythonOperator
-with DAG(
-    'example_ml_dag',
-    description='ML DAG',
+@dag(
     schedule_interval=None,
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
     catchup=False,
-    tags=['piotrek'],
-) as dag:
-    dag.doc_md = __doc__
-    
+    tags=["piotrek"],
+)
+def ml_dag():
     list_tables = PostgresOperator(
         task_id="check_if_table_exists",
         postgres_conn_id="postgres_default",
         sql="select * from pg_catalog.pg_tables;",
     )
-
-    @tasks
+ 
+    @task()
     def check_if_table_exists(table_list, table: str):
         print(table_list)
         print(type(table_list))
